4f673e86fa30d267d837c260fc59363a33124a0c
JohnT42 <j.tran24@hotmail.com>
2018-11-16 11:18:43 +0000

    Second commit with the domain model implemented

---
+++ src/main/java/com/qa/persistance/domain/Classroom.java
@@ -0,0 +1,52 @@
+package com.qa.persistance.domain;
+
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Classroom {
+	public Classroom() {
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long classroomID;
+
+	@Column(length = 100)
+	private String trainer;
+
+	@OneToMany(mappedBy="trainerTrainee")
+	private List<Trainee> trainees;
+
+	public Long getClassroomID() {
+		return classroomID;
+	}
+
+	public void setClassroomID(Long classroomID) {
+		this.classroomID = classroomID;
+	}
+
+	public String getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(String trainer) {
+		this.trainer = trainer;
+	}
+
+	public List<Trainee> getTrainees() {
+		return trainees;
+	}
+
+	public void setTrainees(List<Trainee> trainees) {
+		this.trainees = trainees;
+	}
+
+	
+
+}
